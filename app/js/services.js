3bbe5dce32387a1eb962adedbd6ec9d92e879ae3
Teisia Park <teisia@gmail.com>
2016-03-06 19:19:15 -0700

    added trail route but not succesffuly rendering

--- app/js/services.js
+++ app/js/services.js
@@ -2,17 +2,17 @@ app.service("fourteenerService", function($http){
   var fourteenerService = {};
 
   fourteenerService.getFourteeners = function(){
-    return $http.get("http://localhost:3000/14ers", {method: "jsponp"});
+    //return $http.get("http://localhost:3000/14ers", {method: "jsponp"});
     return $http.get("https://api-14er.herokuapp.com/14ers", {method: "jsonp"});
   }
 
   fourteenerService.getFourteener = function(fourteener_id){
-    return $http.get("http://localhost:3000/14ers/"+fourteener_id);
+    //return $http.get("http://localhost:3000/14ers/"+fourteener_id);
     return $http.get("https://api-14er.herokuapp.com/14ers/"+fourteener_id, {method: "jsonp"});
   }
 
   fourteenerService.getTrails = function(fourteener_id){
-    return $http.get("http://localhost:3000/14ers/"+fourteener_id+"/trails");
+    //return $http.get("http://localhost:3000/14ers/"+fourteener_id+"/trails");
     return $http.get("https://api-14er.herokuapp.com/14ers/"+fourteener_id+"/trails", {method: "jsonp"});
   }
 
